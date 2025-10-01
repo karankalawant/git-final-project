edff59e3da00d106beeeabc17c7277d6b7133b6a
karankalawant <karankalawant16@gmail.com>
2025-10-02 01:53:57 +0530

    Delete simple-interest.sh

--- simple-interest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Simple Interest Calculator
-
-read -p "Enter principal amount: " P
-read -p "Enter rate of interest (in %): " R
-read -p "Enter time (in years): " T
-
-SI=$(echo "scale=2; $P * $R * $T / 100" | bc)
-echo "Simple Interest is: $SI"
